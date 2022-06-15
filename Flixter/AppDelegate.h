f9aa6fecc4fd269fdc1f93339d9c447ad7c47b63
Jake Torres <jakert2244@gmail.com>
2022-06-15 10:21:16 -0700

    Initial Commit

---
+++ Flixter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flixter
+//
+//  Created by Jake Torres on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
